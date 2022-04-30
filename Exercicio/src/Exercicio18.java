28ce0dff16ac48a8ee82e73acee5da982a895ea1
Giovani Zanella Da Maia <gzmaia@furb.br>
2022-04-29 21:35:59 -0300

    eaercicio

---
+++ Exercicio/src/Exercicio18.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o numero de horas trabalhadas: ");
+        double salarioph=in.nextDouble();
+
+        
+
+    }
+}
--- Exercicio/src/Exercico12.java
+++ Exercicio/src/Exercico12.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class Exercicio11 {
+public class Exercico12 {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Digite o valor ");
         double valor = in.nextDouble();
 
-        if (valor > 10) {
-            System.out.println("Maior que 10!");
+        if (valor > 0) {
+            System.out.println("o valor é positivo");
         } else {
-            System.out.println("Menor que 10!");
+            System.out.println("o valor é negativo");
         }
     }
 
