c28e28cc36b97f7cb2fbce757700b11a1d1ebb5b
Giovani Zanella Da Maia <gzmaia@furb.br>
2022-05-19 21:47:35 -0300

    codigo morse

--- StringseArrays/src/Exemplos.java
+++ StringseArrays/src/Exemplos.java
@@ -2,14 +2,14 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
 public class Exemplos {
-
+    public static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         //exemplo1();
         //exemplo2();
         //exemplo3();
         //exemplo4();
         //exemplo5();
-       // exemplo6();
+        // exemplo6();
         //exemplo7();
         exemplo8();
 
@@ -18,104 +18,120 @@ public class Exemplos {
     public static void exemplo1() {
 
     }
+
     public static void exemplo2() {
-        Scanner in =new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
         System.out.println("Digite alguma coisa: ");
-        String algumacoisa=in.nextLine();
+        String algumacoisa = in.nextLine();
         System.out.println("Digite alguma outra coisa: ");
-        String algumaOutracoisa=in.nextLine();
+        String algumaOutracoisa = in.nextLine();
 
         //equald mostra se é Exatamente igual ex: se comecar com maiuscula ele so vai ser verdadeiro se o que ele quer verificar comecar com maiuscula
-        if (algumacoisa.equals(algumaOutracoisa)){
+        if (algumacoisa.equals(algumaOutracoisa)) {
             System.out.println("São exatamente iguais");
-        // o equalsignoreCase te mostra se igual independente se é maiuscula ou minuscula
+            // o equalsignoreCase te mostra se igual independente se é maiuscula ou minuscula
         } else if (algumacoisa.equalsIgnoreCase(algumaOutracoisa)) {
             System.out.println("São iguais independente da case");
-        }else {
+        } else {
             System.out.println("São diferentes");
         }
 
     }
+
     public static void exemplo3() {
-        Scanner in =new Scanner(System.in);
-
+        //Scanner in = new Scanner(System.in);
         System.out.println("Digite alguma coisa: ");
-        String algumacoisa=in.nextLine();
+        String algumacoisa = in.nextLine();
         System.out.println("Digite alguma outra coisa: ");
-        String algumaOutracoisa=in.nextLine();
+        String algumaOutracoisa = in.nextLine();
 
-        if(algumacoisa.contains(algumaOutracoisa)){
-            System.out.println(algumaOutracoisa+" faz parte de "+algumacoisa);
-        }else if (algumaOutracoisa.contains(algumacoisa)){
-            System.out.println(algumaOutracoisa+" faz parte de "+algumacoisa);
-        }else {
+        if (algumacoisa.contains(algumaOutracoisa)) {
+            System.out.println(algumaOutracoisa + " faz parte de " + algumacoisa);
+        } else if (algumaOutracoisa.contains(algumacoisa)) {
+            System.out.println(algumaOutracoisa + " faz parte de " + algumacoisa);
+        } else {
             System.out.println("nenhuma Strig faz parte da outra");
 
         }
-}
-    public static void exemplo4() {
-        Scanner in =new Scanner(System.in);
+    }
 
+    public static void exemplo4() {
+        //Scanner in = new Scanner(System.in);
         System.out.println("Digite alguma coisa: ");
-        String algumacoisa=in.nextLine();
+        String algumacoisa = in.nextLine();
         System.out.println("Digite alguma outra coisa: ");
-        String algumaOutracoisa=in.nextLine();
+        String algumaOutracoisa = in.nextLine();
 
         //o Start With verifica se uma String faz parte da outra e essa parte é o começo
-       if(algumacoisa.startsWith(algumaOutracoisa)){
+        if (algumacoisa.startsWith(algumaOutracoisa)) {
             System.out.println(algumacoisa + " é prefixo de " + algumaOutracoisa);
-        }else if (algumaOutracoisa.startsWith(algumacoisa)){
-           System.out.println(algumaOutracoisa + " é prefixo de " + algumacoisa);
-        }else {
-           System.out.println("As strings não são prefxo uma da outra");
-       }
-
+        } else if (algumaOutracoisa.startsWith(algumacoisa)) {
+            System.out.println(algumaOutracoisa + " é prefixo de " + algumacoisa);
+        } else {
+            System.out.println("As strings não são prefxo uma da outra");
         }
-    public static void exemplo5() {
-        Scanner in =new Scanner(System.in);
 
+    }
+
+    public static void exemplo5() {
+        //Scanner in = new Scanner(System.in);
         System.out.println("Digite alguma coisa: ");
-        String algumacoisa=in.nextLine();
+        String algumacoisa = in.nextLine();
         System.out.println("Digite alguma outra coisa: ");
-        String algumaOutracoisa=in.nextLine();
+        String algumaOutracoisa = in.nextLine();
 
         //o endsWit verifica se uma String faz parte da outra e essa parte é o final
-        if(algumacoisa.endsWith(algumaOutracoisa)){
+        if (algumacoisa.endsWith(algumaOutracoisa)) {
             System.out.println(algumacoisa + " é sufixo de " + algumaOutracoisa);
-        }else if (algumaOutracoisa.endsWith(algumacoisa)){
+        } else if (algumaOutracoisa.endsWith(algumacoisa)) {
             System.out.println(algumaOutracoisa + " é sufixo de " + algumacoisa);
-        }else {
+        } else {
             System.out.println("As strings não são sufixo uma da outra");
         }
-
     }
-    public static void exemplo6() {
-        Scanner in = new Scanner(System.in);
 
+    public static void exemplo6() {
+        //Scanner in = new Scanner(System.in);
         System.out.println("Digite alguma coisa: ");
-        String algumacoisa=in.nextLine();
+        String algumacoisa = in.nextLine();
         System.out.println("Digite alguma outra coisa: ");
-        String algumaOutracoisa=in.nextLine();
+        String algumaOutracoisa = in.nextLine();
 
         //retorna o indice da primeira vez de  um valor e mostra a posição desse valor
-        int posicao=algumacoisa.indexOf(algumaOutracoisa);
-        System.out.println("A posição de "+algumaOutracoisa+" em "+algumacoisa+" é "+posicao);
+        int posicao = algumacoisa.indexOf(algumaOutracoisa);
+        System.out.println("A posição de " + algumaOutracoisa + " em " + algumacoisa + " é " + posicao);
     }
+
     public static void exemplo7() {
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
         //replace recebe 2 valores replace( valor q vamos substituir , valor a ser posto no valor a ser substituido)
-
-        String cyphered="lala#lwve#-rwgra22lalang!#<3";
-        String decyphered =cyphered.replace("w","o");
-        decyphered=decyphered.replace("#"," ");
-        decyphered=decyphered.replace("2","m");
-        decyphered=decyphered.replace("lala","i");
-        decyphered=decyphered.replace("-","p");
+        String cyphered = "lala#lwve#-rwgra22lalang!#<3";
+        String decyphered = cyphered.replace("w", "o");
+        decyphered = decyphered.replace("#", " ");
+        decyphered = decyphered.replace("2", "m");
+        decyphered = decyphered.replace("lala", "i");
+        decyphered = decyphered.replace("-", "p");
         System.out.println(decyphered);
-
     }
+
+    //Código Morse
     public static void exemplo8() {
-        Scanner in = new Scanner(System.in);
-}}
+        System.out.print("Digite uma expressão encriptada com código morse: ");
+        String palavra= in.nextLine();
+        String decifrado = palavra.replace(".----","1").replace("..---","2").replace("...--","3")
+                .replace("....-","4").replace(".....","5").replace("-....","6")
+                .replace("--...","7").replace("---..","8").replace("----.","9")
+                .replace("-----","0").replace("-...","b").replace("-.-.","c")
+                .replace("..-.","f").replace("....","h").replace(".---","j")
+                .replace(".-..","l").replace(".--.","p").replace("--.-","q")
+                .replace("...-","v").replace("-..-","x").replace("-.--","y")
+                .replace("--..","z").replace("-..","d").replace("--.","g")
+                .replace("-.-","k").replace("---","o").replace(".-.","r")
+                .replace("...","s").replace("..-","u").replace(".--","w")
+                .replace(".-","a").replace("..","i").replace("--","m")
+                .replace("-.","n").replace(".","e").replace("-","t");
+        System.out.println(decifrado);
+    }
+}
 
 
