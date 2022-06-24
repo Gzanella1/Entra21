0bea01bfc4c83247e90cb1cd6fa74cdff4d90176
Giovani Zanella Da Maia <gzmaia@furb.br>
2022-06-23 21:52:52 -0300

    refazer a atividade voo

--- ExercicioJavaAvancado/src/Main.java
+++ ExercicioJavaAvancado/src/Main.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner in = new Scanner(System.in);
+    static HashMap<String, ArrayList<Carro>> carros = new HashMap<>();
 
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Carro>> carros = new HashMap<>();
-
         boolean executa = true;
 
         while (executa) {
@@ -21,45 +21,91 @@ public class Main {
             in.nextLine();
 
             switch (escolha) {
-                case 0: executa =false; break;
-                case 1:
-                    if(carros.isEmpty()){
-                        System.out.println("Não existem carros cadastrados!");
-                    }else {
-
-                    }
-
-                case 2:
-                    Carro c = new Carro();
-                    System.out.println("Informe o modelo do  carro: ");
-                    c.setModelo(in.nextLine());
-                    System.out.println("informe a Marca de venda");
-                    String marca = in.nextLine();
-                    System.out.println("informe o valor do veiculo: ");
-                    double valor = in.nextDouble();
-                    System.out.println("informe o valor de venda: ");
-                    c.setValorVenda(in.nextDouble());
-                    if (!carros.containsKey(marca)) {
-                        carros.put(marca, new ArrayList<>());
-                    }
-                    carros.get(marca).add(c);
-                    System.out.println("carro adicionado");
-                    System.out.println();
+                case 0:
+                    executa = false;
                     break;
-
+                case 1:
+                    verLista();
+                    break;
+                case 2:
+                    addumcarro();break;
                 case 3:
-                    if (carros.isEmpty()) {
-                        System.out.println("Não há Carros cadastrdos");
-                    } else {
-                        System.out.println("Qual carro deseja vender:");
-                        String busca = in.nextLine().toLowerCase();
-                        if (carros.containsKey(busca)) {
-                            System.out.println("");
-                        }
-                        break;
-                    }
+                    venderumcarro();break;
+                default:
+                    System.out.println("invalido");
+            }
+        }
+
+    }
+
+    public static void verLista() {
+        //verifica se há algum carro no Hashmap
+        if (carros.isEmpty()) {
+            System.out.println("Não há Carros cadastrados");
+        } else {
+            System.out.println("--------CATALAGO----------- ");
+            //keySet retorna as chaves do hashMap no caso a marca
+            //ou seja ele so busca chave por chave
+            for (String key : carros.keySet()) {
+                System.out.println(key.toUpperCase());
+                //recupra os items da string key
+                //criou uma instancia da classe carro
+                //busca o modelo DA array
+                for (Carro separapormodelo : carros.get(key)) {
+                    //ver o que é isso "%s: R$%.2f\n",
+                    System.out.printf("%s: R$%.2f\n", separapormodelo.getModelo(), separapormodelo.getValorVenda());
+                }
             }
         }
     }
-}
+    public static void addumcarro(){
+        Carro c = new Carro();
+        System.out.println("Informe o MODELO do  carro: ");
+        c.setModelo(in.nextLine());
+        System.out.println("informe a MARCA de venda");
+        String marca = in.nextLine();
+        System.out.println("informe o valor de venda: ");
+        c.setValorVenda(in.nextDouble());
+        in.nextLine();
+        if (!carros.containsKey(marca)) {
+            //não entendi
+            carros.put(marca, new ArrayList<>());
+        }
+        //add um carro no arraylist
+        carros.get(marca).add(c);
+        System.out.println("carro adicionado");
+        System.out.println();
+    }
+    public static void venderumcarro(){
+            System.out.print("Informe a marca do carro: ");
+            String marca = in.nextLine();
+            if (!carros.containsKey(marca.toLowerCase())) {
+                System.out.println("Não existem carros da marca '"+marca+"'!");
+            } else {
+                System.out.print("Informe o modelo do carro: ");
+                String modelo = in.nextLine();
+                ArrayList<Carro> carrosMarca = carros.get(marca.toLowerCase());
+                boolean encontrado = false;
+                for (int i = 0; i < carrosMarca.size(); i++) {
+                    Carro c = carrosMarca.get(i);
+                    if (c.getModelo().equalsIgnoreCase(modelo)) {
+                        encontrado = true;
+                        System.out.printf("Valor da venda: R$%.2f. Deseja efetuar venda (S/N)? ", c.getValorVenda());
+                        if (in.nextLine().equalsIgnoreCase("S")) {
+                            System.out.printf("Carro %s da marca %s vendido por R$%.2f!\n", c.getModelo(), marca, c.getValorVenda());
+                            carrosMarca.remove(i);
+                            if (carrosMarca.isEmpty()) {
+                                carros.remove(marca.toLowerCase());
+                            }
+                        }
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    System.out.println("Carro de modelo '"+modelo+"' não encontrado na marca '"+marca+"'!");
+                }
+            }
+            System.out.println();
+        }
+    }
 
--- ProgramacaoOrientadaObjeto/src/classes/lanche/Sanduiche.java
+++ ProgramacaoOrientadaObjeto/src/classes/lanche/Sanduiche.java
@@ -1,17 +1,20 @@
 package classes.lanche;
 
+import org.w3c.dom.ls.LSException;
+
+import javax.crypto.spec.PSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public abstract class Sanduiche extends Lanche{
+public abstract class Sanduiche extends Lanche {
+    private HashMap<String, Double> adicionais = new HashMap<>();
 
-    private ArrayList<String> adicionais = new ArrayList<>();
-
-    public void adicionarAdicional(String adicional) {
-        this.adicionais.add(adicional);
+    public void adicionarAdicional(String adicional, double valor) {
+        this.adicionais.put(adicional, valor);
     }
 
-    public ArrayList<String> getAdicionais() {
+    public HashMap<String, Double> getAdicionais() {
         return adicionais;
     }
 
@@ -20,8 +23,8 @@ public abstract class Sanduiche extends Lanche{
         System.out.println("====" + this.getTipo() + "====");
         if (!this.adicionais.isEmpty()) {
             System.out.println("-ADICIONAIS-");
-            for (String adicional : this.getAdicionais()) {
-                System.out.println(adicional);
+            for (String adicional : this.getAdicionais().keySet()) {
+                System.out.printf("%s: R$.2f\n", adicional, this.getAdicionais().get(adicional));
             }
         }
     }
@@ -31,9 +34,13 @@ public abstract class Sanduiche extends Lanche{
         System.out.println("Deseja adicionais? (S/N)");
         String adiconais = in.nextLine();
         if (adiconais.equalsIgnoreCase("S")) {
-            for(int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {
                 System.out.print("Informe o adicional: ");
-                this.adicionarAdicional(in.nextLine());
+                System.out.println("informe um adicional: ");
+                String nomeAdicional = in.nextLine();
+                System.out.println("informe o valor do adiciona: ");
+                this.adicionarAdicional(nomeAdicional, in.nextDouble());
+                in.nextLine();
                 System.out.println("Deseja adicionar mais adicionais? (S/N)");
                 String parada = in.nextLine();
                 if (parada.equalsIgnoreCase("N")) {
@@ -42,14 +49,18 @@ public abstract class Sanduiche extends Lanche{
             }
         }
     }
+
+    @Override
+    public double getValor() {
+     double valorToral=super.getValor();
+        for (Double v : this.adicionais.values()) {
+            valorToral+=v;
+        }
+        return valorToral;
+    }
 }
 //USE O SHIFT + ALT  E SELECIONA AS LINHAS QUE VOCE QUE DIGITAR AO MESMO TEMPO
 //CONTROL + CLICA EM CIMA DO QUE VOCE QUE PROCURAR
-
-
-
-
-
 
 
 
