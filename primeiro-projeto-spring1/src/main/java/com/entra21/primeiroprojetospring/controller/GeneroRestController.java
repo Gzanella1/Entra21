e7a9e243e1600887e3d14edd3109d7ad74c77182
Giovani Zanella Da Maia <gzmaia@furb.br>
2022-09-06 21:41:35 -0300

    spring

--- primeiro-projeto-spring1/src/main/java/com/entra21/primeiroprojetospring/controller/GeneroRestController.java
+++ primeiro-projeto-spring1/src/main/java/com/entra21/primeiroprojetospring/controller/GeneroRestController.java
@@ -1,7 +1,13 @@
 package com.entra21.primeiroprojetospring.controller;
 
+import com.entra21.primeiroprojetospring.model.dto.FranquiaDTO;
+import com.entra21.primeiroprojetospring.model.dto.FranquiaPayloadDTO;
+import com.entra21.primeiroprojetospring.model.dto.GeneroDTO;
+import com.entra21.primeiroprojetospring.model.dto.GeneroPayLoadDTO;
 import com.entra21.primeiroprojetospring.model.entity.GeneroEntity;
 import com.entra21.primeiroprojetospring.view.repository.GeneroRepository;
+import com.entra21.primeiroprojetospring.view.service.FranquiaService;
+import com.entra21.primeiroprojetospring.view.service.GeneroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,42 +20,30 @@ import java.util.Optional;
 public class GeneroRestController {
 
     @Autowired
-    private GeneroRepository generoRepository;
-    
+    private GeneroService generoService;
+
     @GetMapping
-    public List<GeneroEntity> getGeneros() {
-        return generoRepository.findAll();
+    public List<GeneroDTO> getFranquias() {
+        return generoService.getAll();
     }
 
     @PostMapping
-    public void addFranquia(@RequestBody GeneroEntity entity) {
-        generoRepository.save(entity);
+    public void addGenero (@RequestBody GeneroPayLoadDTO newGenero) {
+        generoService.save(newGenero);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GeneroEntity> getFranquia(@PathVariable(name = "id") Long id) {
-        Optional<GeneroEntity> franquia = generoRepository.findById(id);
-        if (franquia.isPresent()) {
-            return ResponseEntity.ok(franquia.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public GeneroDTO getGenero(@PathVariable(name = "id") Long id) {
+        return generoService.getById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteFranquia(@PathVariable(name = "id") Long id) {
-        generoRepository.deleteById(id);
+    public void deleteGenero(@PathVariable(name = "id") Long id) {
+        generoService.delete(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<GeneroEntity> updateFranquia(@PathVariable(name = "id") Long id,
-                                                         @RequestBody String novoNome) {
-        Optional<GeneroEntity> entity = generoRepository.findById(id);
-        if (entity.isPresent()) {
-            entity.get().setNome(novoNome);
-            return ResponseEntity.ok(generoRepository.save(entity.get()));
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public GeneroDTO updateGenero(@PathVariable(name = "id") Long id, @RequestBody String novoNome) {
+        return generoService.update(id, novoNome);
     }
 }
---
+++ primeiro-projeto-spring1/src/main/java/com/entra21/primeiroprojetospring/view/service/GeneroService.java
@@ -0,0 +1,60 @@
+package com.entra21.primeiroprojetospring.view.service;
+
+import com.entra21.primeiroprojetospring.model.dto.FranquiaDTO;
+import com.entra21.primeiroprojetospring.model.dto.FranquiaPayloadDTO;
+import com.entra21.primeiroprojetospring.model.dto.GeneroDTO;
+import com.entra21.primeiroprojetospring.model.dto.GeneroPayLoadDTO;
+import com.entra21.primeiroprojetospring.model.entity.FranquiaEntity;
+import com.entra21.primeiroprojetospring.model.entity.GeneroEntity;
+import com.entra21.primeiroprojetospring.view.repository.GeneroRepository;
+import jdk.jshell.execution.LoaderDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GeneroService {
+
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    public List<GeneroDTO> getAll() {
+        return generoRepository.findAll().stream().map(gr -> {
+            GeneroDTO dto = new GeneroDTO();
+            dto.setNome(gr.getNome());
+            dto.setId(gr.getId());
+            return dto;
+        }).collect(Collectors.toList());
+    }
+
+    public void save(GeneroPayLoadDTO input) {
+        GeneroEntity newEntity = new GeneroEntity();
+        newEntity.setNome(input.getNome());
+        generoRepository.save(newEntity);
+    }
+
+    public void delete(Long id) {
+        generoRepository.deleteById(id);
+    }
+
+    public GeneroDTO getById(Long id) {
+        GeneroEntity e = generoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Genero não encontrada!"));
+        GeneroDTO dto = new GeneroDTO();
+        dto.setId(e.getId());
+        dto.setNome(e.getNome());
+        return dto;
+    }
+    public GeneroDTO update(Long id, String novoNome) {
+        GeneroEntity e = generoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Genero não encontrada!"));
+        e.setNome(novoNome);
+        e = generoRepository.save(e);
+        GeneroDTO dto = new GeneroDTO();
+        dto.setNome(e.getNome());
+        dto.setId(e.getId());
+        return dto;
+    }
+}
